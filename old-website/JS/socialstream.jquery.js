8579060b0efb44754a3db2af8d9f3a7496429c73
Noman Sadiq <noman.sadiq@maf.ae>
2024-02-24 19:48:25 +0400

    old website backup

---
+++ old-website/JS/socialstream.jquery.js
@@ -0,0 +1,14 @@
+(function ($) {
+    $.fn.socialstream = function (options) {
+        var defaults = {
+            socialnetwork: 'flickr',
+            username: 'pixel-industry',
+            limit: 6,
+            overlay: true,
+            apikey: false
+        };
+        var options = $.extend(defaults, options);
+
+        
+    };
+})(jQuery);
